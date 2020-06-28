3e014698f52dbb892df4ca60ae3a671fefa7561d
Edson Gomes <edyz86@hotmail.com>
2020-06-27 23:00:05 -0300

    Done

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -2,9 +2,15 @@ import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
 import CreateProductService from '@modules/products/services/CreateProductService';
+import Product from '../../typeorm/entities/Product';
 
 export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, price, quantity } = request.body;
+
+    const createProduct = container.resolve(CreateProductService);
+    const product = await createProduct.execute({ name, price, quantity });
+
+    return response.json(product);
   }
 }
